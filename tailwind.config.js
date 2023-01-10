e477386b547de546afb10c5c0dad318a2435e57b
romelnadz <76258692+romelnadz@users.noreply.github.com>
2023-01-10 11:12:30 +0800

    new home nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,28 @@ module.exports = {
       {
         work_sans: ["Work Sans", "sans-serif"],
       },
+      spacing: {
+        '10vw': '10vw',
+        '20vw': '20vw',
+        '30vw': '30vw',
+        '40vw': '40vw',
+       
+      }
     },
   },
   plugins: [
     require('@tailwindcss/forms')
   ],
 }
+// '1': '8px',
+// '2': '12px',
+// '3': '16px',
+// '4': '24px',
+// '5': '32px',
+// '6': '48px',
+// '7': '48px',
+// '8': '12px',
+// '9': '16px',
+// '10': '24px',
+// '11': '32px',
+// '12': '48px',
