a4ca03314a9e38f2a6b426bd1fab0e172d111d9e
romelnadz <76258692+romelnadz@users.noreply.github.com>
2023-01-12 17:09:26 +0800

    zxcz

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        // 'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        // 'laptop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        'desktop': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
       colors: 
       {
         fc_red: '#BF1901',
@@ -34,6 +50,21 @@ module.exports = {
         '160vw': '160vw',
         '180vw': '180vw',
         'm_xl': '200px',
+        'mobile_64px': '17vw',
+        'laptop_64px': '3.7vw',
+
+        // PERCENT
+        '10%': '10%',
+        '20%': '20%',
+        '30%': '30%',
+        '40%': '40%',
+        '50%': '50%',
+        '60%': '60%',
+        '70%': '70%',
+        '80%': '80%',
+        '90%': '90%',
+        '100%': '100%',
+        
        
       }
     },
