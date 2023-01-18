5412a53f617afaa2d6246d02597f0d3ac39c7b67
romelnadz <76258692+romelnadz@users.noreply.github.com>
2023-01-18 14:59:36 +0800

    jan18

--- css/carousel.js
+++ css/carousel.js
@@ -219,7 +219,7 @@ var Carousel = function ($$$1) {
   var TOUCHEVENT_COMPAT_WAIT = 500; // Time for mouse compat events to fire after touch
 
   var Default = {
-    interval: 5000,
+    interval: 3000,
     keyboard: true,
     slide: false,
     pause: 'hover',
